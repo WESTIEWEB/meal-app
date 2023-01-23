2a211fb76842fb5861b6bd1e9bb95e011ffd3e70
Chibuike Ilo <82292997+WESTIEWEB@users.noreply.github.com>
2023-01-23 23:55:40 +0100

    meal-mart-app

---
+++ meal-mart/src/App.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+import Meals from './Components/Meals/Meals';
+// import Modal from './Components/Modal/Modal';
+// import Favorites from './Components/Favorites/Favorites';
+// import Search from './Components/Search/Search';
+
+function App() {
+  return (
+    <>
+      <Meals />
+      {/* <Modal />
+      <Favorites />
+      <Search/> */}
+    </>
+  );
+}
+
+export default App;
